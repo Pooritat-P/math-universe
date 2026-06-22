5984a17f1b0b6f158eb9405ba5037448575a3767
Pooritat-P <spatan18799@gmail.com>
2026-06-22 23:36:29 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='math-universe-complete-edition-v2';
+const CACHE_NAME='math-universe-complete-edition-v3';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./assets/ui/hero_complete_edition.png','./assets/infographic/development_story.png','./assets/infographic/user_guide.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
